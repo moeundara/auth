61c3aafd22dcccb2e1770fcc269953643ce531fe
moeundara <moeundara@gmail.com>
2021-01-19 12:31:34 +0700

    Add env key

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+// const loader = document.getElementById("loader");
+// const container = document.getElementById("jumbotron");
+//
+// function loading(){
+//   loader.hidden=false;
+//   container.hidden = true;
+// }
+// function completeLoading(){
+//   if(!loader.hidden){
+//     loader.hidden=true;
+//     container.hidden=false;
+//   }
+// }
